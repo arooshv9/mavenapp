c8a8261245f32eed65d450fdce8c64e214fc9bcb
arooshv9 <arooshreddy9999@gmail.com>
2024-12-04 10:44:31 +0530

    Initial commit

---
+++ src/main/java/maven_java/com/bd1a663l/App.java
@@ -0,0 +1,13 @@
+package maven_java.com.bd1a663l;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
